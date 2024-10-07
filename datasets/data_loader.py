8ae96e1bf1e9891c8932b424ca285c9c40edb013
thang viet doan <tdoan011@r-mac-0156.cs.fiu.edu>
2024-10-07 15:05:57 -0400

    update: dataset

---
+++ datasets/data_loader.py
@@ -0,0 +1,291 @@
+import json
+import pandas as pd
+import os
+
+class DataLoader():
+    def load_dataset_winobias():
+        files = ['anti_stereotyped_type1.txt.dev', 'anti_stereotyped_type1.txt.test', 'anti_stereotyped_type2.txt.dev', 'anti_stereotyped_type2.txt.test',
+         'pro_stereotyped_type1.txt.dev', 'pro_stereotyped_type1.txt.test', 'pro_stereotyped_type2.txt.dev', 'pro_stereotyped_type2.txt.test']
+
+        sentences = []
+
+        for file in files:
+            file_path = f'./data/WinoBias/data/{file}'
+            with open(file_path, 'r', encoding='utf-8') as f:
+                sentences += f.readlines()
+
+        return sentences
+    
+    def load_dataset_winobiasplus():
+        sentences = []
+        file_path = f'./data/WinoBias+/data/WinoBias+.txt'
+        with open(file_path, 'r', encoding='utf-8') as f:
+            sentences += f.readlines()
+
+        return sentences
+    
+    def load_dataset_winogender():
+        df = pd.read_csv('./data/WinoGender/data/all_sentences.tsv', sep='\t')
+        sentences = df['sentence'].tolist()
+
+        return sentences
+        
+    def load_dataset_gap():
+        files = ['gap-development.tsv', 'gap-test.tsv', 'gap-validation.tsv']
+
+        sentences = []
+
+        for file in files:
+            df = pd.read_csv(f'./data/GAP/data/{file}', sep='\t')
+
+            sentence = df['Text'].tolist()
+            sentences += sentence
+
+        return sentences
+
+    def load_dataset_bug():
+        files = ['balanced_BUG.csv', 'full_BUG.csv', 'gold_BUG.csv']
+
+        sentences = []
+
+        for file in files:
+            df = pd.read_csv(f'./data/BUG/data/{file}')
+
+            sentence = df['sentence_text'].tolist()
+            sentences += sentence
+        
+        return sentences
+    
+    def load_dataset_stereset():
+        files = ["./data/StereoSet/data/dev.json", "./data/StereoSet/data/test.json"]
+        all_sentences = []
+
+        for file in files:
+            with open(file, 'r') as file:
+                data = json.load(file)
+
+                for key in ['intersentence', 'intrasentence']:
+                    if key in data['data']:
+                        for entry in data['data'][key]:
+                            context = entry.get('context', "")
+                            if context:
+                                all_sentences.append(context)
+                            
+                            # Extract sentences from each entry
+                            sentences = entry.get('sentences', [])
+                            for sentence_data in sentences:
+                                sentence = sentence_data.get('sentence', "")
+                                if sentence:
+                                    all_sentences.append(sentence)
+
+        return all_sentences
+
+    def load_dataset_becpro():
+        df = pd.read_csv(f'./data/BEC-Pro/data/BEC-Pro_EN.tsv', sep='\t')
+        sentences = df['Sentence'].tolist()
+
+        return sentences
+
+    def load_dataset_honest():
+        files = ['binary/en_template.tsv', 'queer_nonqueer/en_template.tsv']
+
+        sentences = []
+
+        for file in files:
+            df = pd.read_csv(f'./data/HONEST/data/{file}', sep='\t')
+
+            sentence = df['template_masked'].tolist()
+            sentences += sentence
+
+        return sentences
+
+    def load_dataset_crowspairs():
+        sentences = []
+        df = pd.read_csv(f'./data/CrowS-Pairs/data/crows_pairs_anonymized.csv')
+
+        sentence = df['sent_more'].tolist()
+        sentences += sentence
+        sentence = df['sent_less'].tolist()
+        sentences += sentence
+
+        return sentences
+
+    def load_dataset_eec():
+        df = pd.read_csv(f'./data/EEC/data/Equity-Evaluation-Corpus.csv')
+        sentences = df['Sentence'].tolist()
+
+        return sentences
+
+    def load_dataset_panda():
+        directory_path = "./data/PANDA/data"  
+
+        json_files = []
+        for root, dirs, files in os.walk(directory_path):
+            for file in files:
+                if file.endswith(".jsonl"):
+                    json_files.append(os.path.join(root, file))
+
+        sentences = []
+
+        for file in json_files:
+            with open(file, 'r', encoding='utf-8') as file:
+                for line in file:
+                    # Parse each line as a JSON object
+                    data = json.loads(line)
+                        
+                    # Extract 'original' and 'rewrite' fields
+                    original = data.get('original', '')
+                    rewrite = data.get('rewrite', '')
+                        
+                    # Add the extracted fields to the list as a tuple
+                    sentences.append(original)
+                    sentences.append(rewrite)
+            
+            return sentences
+
+    def load_dataset_redditbias():
+        directory_path = "./data/RedditBias/data"  
+
+        json_files = []
+        for root, dirs, files in os.walk(directory_path):
+            for file in files:
+                if file.endswith(".json"):
+                    json_files.append(os.path.join(root, file))
+
+        sentences = []
+        for file in json_files:
+            df = pd.read_csv(file)
+
+            sentence = df['comments_processed'].tolist()
+            sentences += sentence
+
+        return sentences
+
+    def load_dataset_winoqueer():
+        sentences = []
+        df = pd.read_csv(f'./data/WinoQueer/data/winoqueer_final.csv')
+        sentence = df['sent_x'].tolist()
+        sentences += sentence
+        sentence = df['sent_y'].tolist()
+        sentences += sentence
+
+        return sentences
+
+    def load_dataset_fairprism():
+        files = ['fairprism_aggregated.csv', 'fairprism_disaggregated.csv']
+
+        sentences = []
+
+        for file in files:
+            df = pd.read_csv(f'./data/FairPrism/data/{file}')
+
+            sentence = df['Human Input'].tolist()
+            sentences += sentence
+            sentence = df['AI Output'].tolist()
+            sentences += sentence
+        
+        return sentences
+
+    def load_dataset_bold():
+        files = ['gender_prompt.json',
+                'political_ideology_prompt.json',
+                'profession_prompt.json',
+                'race_prompt.json',
+                'religious_ideology_prompt.json']
+
+        sentences = []
+
+        for file in files:
+            with open(f'./data/BOLD/data/prompts/{file}', 'r') as file:
+                data = json.load(file)
+
+                # Extract sentences from all categories in the dataset
+                cur_sentences = [sentence for category in data.values() for actor_info in category.values() for sentence in actor_info]
+                sentences += cur_sentences
+        
+        return sentences
+
+    def load_dataset_holisticbias():
+        df = pd.read_csv(f'./data/HolisticBias/data/data/sentences.csv')
+
+        sentences = df['text'].tolist()
+
+        return sentences
+
+    def load_dataset_bbq():
+        directory_path = "./data/BBQ/data"  
+
+        json_files = []
+        for root, dirs, files in os.walk(directory_path):
+            for file in files:
+                if file.endswith(".jsonl"):
+                    json_files.append(os.path.join(root, file))
+
+        sentences = []
+
+        for file in json_files:
+            with open(file, 'r', encoding='utf-8') as file:
+                for line in file:
+                    data = json.loads(line)
+                        
+                    context = data.get('context', '')
+                    question = data.get('question', '')
+                        
+                    sentences.append(context)
+                    sentences.append(question)
+        return sentences
+
+    def load_dataset_unqover():
+        directory_path = "./data/UnQover/data"  
+
+        json_files = []
+        for root, dirs, files in os.walk(directory_path):
+            for file in files:
+                if file.endswith(".json"):
+                    json_files.append(os.path.join(root, file))
+
+        sentences = []
+
+        for file in json_files:
+            with open(file, 'r', encoding='utf-8') as f:
+
+                    data = json.load(f)
+                        
+                    for key, value in data.items():
+                        context = value.get("context", "")
+                        if "--" in context:
+                            context_before = context.split("--")[0].strip()  
+                            sentences.append(context_before)
+
+                        for q_key in value:
+                            if q_key.startswith("q"):  
+                                question_data = value[q_key]
+                                if "question" in question_data:
+                                    sentences.append(question_data["question"])
+        return sentences
+
+    def load_dataset_ceb():
+        directory_path = "./data/CEB/data"  
+
+        json_files = []
+        for root, dirs, files in os.walk(directory_path):
+            for file in files:
+                if file.endswith(".json"):
+                    json_files.append(os.path.join(root, file))
+
+        sentences = []
+        for file in json_files:
+            with open(f'{file}', 'r', encoding='utf-8') as file:
+                try:
+                    data = json.load(file)
+                        
+                    for prompt_data in data:
+                        sentences.append(prompt_data["prompt"])
+                except json.JSONDecodeError:
+                    print(f"Error decoding JSON from file: {file}")
+
+        return sentences
+
+    
+
+    
---
+++ datasets/tools/main.py
@@ -0,0 +1,240 @@
+import nltk
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import PCA
+from nltk.stem import WordNetLemmatizer
+import matplotlib.pyplot as plt
+import numpy as np
+import string
+import re
+import os
+import spacy
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+# Set the path to your local nltk folder/
+nltk_data_path = os.path.join(os.getcwd(), 'nltk')
+nltk.data.path.append(nltk_data_path)
+
+nlp = spacy.load("en_core_web_sm")
+
+unwanted = {
+    "he", "his", "her", "she", "him", "man", "woman", "men", "women", "spokesman", 
+    "wife", "himself", "son", "mother", "father", "chairman", "daughter", "husband", 
+    "guy", "girls", "girl", "boy", "boys", "brother", "spokeswoman", "female", 
+    "sister", "male", "herself", "brothers", "dad", "actress", "mom", "sons", 
+    "girlfriend", "daughters", "lady", "boyfriend", "sisters", "mothers", "king", 
+    "businessman", "grandmother", "grandfather", "deer", "ladies", "uncle", "males", 
+    "congressman", "grandson", "bull", "queen", "businessmen", "wives", "widow", 
+    "nephew", "bride", "females", "aunt", "prostate cancer", "lesbian", "chairwoman", 
+    "fathers", "moms", "maiden", "granddaughter", "younger brother", "lads", "lion", 
+    "gentleman", "fraternity", "bachelor", "niece", "bulls", "husbands", "prince", 
+    "colt", "salesman", "hers", "dude", "beard", "filly", "princess", "lesbians", 
+    "councilman", "actresses", "gentlemen", "stepfather", "monks", "ex-girlfriend", 
+    "lad", "sperm", "testosterone", "nephews", "maid", "daddy", "mare", "fiancé", 
+    "fiancée", "kings", "dads", "waitress", "maternal", "heroine", "nieces", 
+    "girlfriends", "sir", "stud", "mistress", "lions", "estranged wife", "womb", 
+    "grandma", "maternity", "estrogen", "ex-boyfriend", "widows", "gelding", "diva", 
+    "teenage girls", "nuns", "czar", "ovarian cancer", "countrymen", "teenage girl", 
+    "penis", "bloke", "nun", "brides", "housewife", "spokesmen", "suitors", 
+    "menopause", "monastery", "motherhood", "brethren", "stepmother", "prostate", 
+    "hostess", "twin brother", "schoolboy", "brotherhood", "fillies", "stepson", 
+    "congresswoman", "uncles", "witch", "monk", "viagra", "paternity", "suitor", 
+    "sorority", "macho", "businesswoman", "eldest son", "gal", "statesman", 
+    "schoolgirl", "fathered", "goddess", "hubby", "stepdaughter", "blokes", 
+    "dudes", "strongman", "uterus", "grandsons", "studs", "mama", "godfather", 
+    "hens", "hen", "mommy", "estranged husband", "elder brother", "boyhood", 
+    "baritone", "grandmothers", "grandpa", "boyfriends", "feminism", "countryman", 
+    "stallion", "heiress", "queens", "witches", "aunts", "semen", "fella", 
+    "granddaughters", "chap", "widower", "salesmen", "convent", "vagina", 
+    "beau", "beards", "handyman", "twin sister", "maids", "gals", "housewives", 
+    "horsemen", "obstetrics", "fatherhood", "councilwoman", "princes", "matriarch", 
+    "colts", "ma", "fraternities", "pa", "fellas", "councilmen", "dowry", 
+    "barbershop", "fraternal", "ballerina"}
+
+def get_wordnet_pos(spacy_tag):
+    if spacy_tag in ['ADJ']:
+        return 'a'  # Adjective
+    elif spacy_tag in ['VERB']:
+        return 'v'  # Verb
+    elif spacy_tag in ['NOUN', 'PROPN']:
+        return 'n'  # Noun
+    elif spacy_tag in ['ADV']:
+        return 'r'  # Adverb
+    else:
+        return 'n'  # Default to noun
+
+def remove_punctuation(sentence):
+    """Remove punctuation from a sentence."""
+    translator = str.maketrans('', '', string.punctuation + "[](){}")
+    return sentence.translate(translator)
+
+def remove_numbers(sentence):
+    """Remove numbers from a sentence."""
+    return re.sub(r'\d+', '', sentence)
+
+def preprocess_sentence_for_keywords(sentence):
+    lemmatizer = WordNetLemmatizer()
+    stop_words = set(stopwords.words('english'))
+
+    doc = nlp(sentence)
+    filtered_words_pos = [(token.text, token.pos_) for token in doc if token.pos_ in ['NOUN', 'PROPN', 'PRON', 'ADJ']]
+    lemmatized_words = [lemmatizer.lemmatize(word, get_wordnet_pos(pos)) for word, pos in filtered_words_pos]
+    lemmatized_words = [word.lower() for word in lemmatized_words]
+    lemmatized_words = [remove_numbers(remove_punctuation(word)) for word in lemmatized_words]
+    final_words = [word for word in lemmatized_words if word and word not in stop_words and word.isalnum()]
+
+    return final_words
+
+def cosine_distance(vec1, vec2):
+    vec1 = vec1.flatten()
+    vec2 = vec2.flatten()
+    if np.linalg.norm(vec1) == 0 or np.linalg.norm(vec2) == 0:
+        return 1.0
+    cos_sim = 1 - np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
+    return cos_sim
+
+def generate_keywords_from_sentences(sentences):
+    all_candidates = set()
+
+    sentences = [sentence for sentence in sentences if isinstance(sentence, str) and sentence.strip()]
+
+    for sentence in sentences:
+        preprocessed_words = preprocess_sentence_for_keywords(sentence)
+        
+        all_candidates.update(preprocessed_words)
+    
+    all_candidates = all_candidates.difference(unwanted)
+
+    tfidf_vectorizer = TfidfVectorizer()
+    tfidf_matrix = tfidf_vectorizer.fit_transform(sentences)
+    
+    tfidf_feature_names = tfidf_vectorizer.get_feature_names_out()
+    tfidf_scores = tfidf_matrix.sum(axis=0).tolist()[0]
+    tfidf_scores_dict = dict(zip(tfidf_feature_names, tfidf_scores))
+    
+    candidate_tfidf_scores = {candidate: tfidf_scores_dict.get(candidate, 0) for candidate in all_candidates}
+    top_keywords = sorted(candidate_tfidf_scores.items(), key=lambda x: x[1], reverse=True)[:100]
+    keywords = [word for word, score in top_keywords]
+
+    words_for_bias_calculation = [word for word in tfidf_feature_names if word not in unwanted]
+
+    return words_for_bias_calculation, keywords, tfidf_vectorizer, tfidf_matrix
+
+def visualize_gender_bias_tfidf(sentences, keywords, tfidf_vectorizer, tfidf_matrix):
+    all_candidates = set()
+
+    sentences = [sentence for sentence in sentences if isinstance(sentence, str) and sentence.strip()]
+
+    for sentence in sentences:
+        preprocessed_words = preprocess_sentence_for_keywords(sentence)
+        
+        all_candidates.update(preprocessed_words)
+    
+    all_candidates = all_candidates.difference(unwanted)
+    words_for_bias_calculation = [word for word in all_candidates if word not in unwanted]
+
+    gender_pairs = [("she", "he"), ("her", "his"), ("woman", "man"), ("mary", "john"), ("herself", "himself"), ("daughter", "son"), ("mother", "father"), ("gal", "guy"), ("girl", "boy"), ("female", "male")]
+
+    pair_diffs = []
+    male_vector_list, female_vector_list = [], []
+    for (female_word, male_word) in gender_pairs:
+        try:
+            female_idx = tfidf_vectorizer.vocabulary_[female_word]
+            male_idx = tfidf_vectorizer.vocabulary_[male_word]
+            
+            female_vector = tfidf_matrix[:, female_idx].toarray().flatten()
+            male_vector = tfidf_matrix[:, male_idx].toarray().flatten()
+            
+            diff_vector = female_vector - male_vector
+            pair_diffs.append(diff_vector)
+
+            male_vector_list.append(male_vector)
+            female_vector_list.append(female_vector)
+        except KeyError:
+            print(f"'{female_word}' or '{male_word}' not found in the vocabulary.")
+
+    if len(pair_diffs) > 0:
+        pca = PCA(n_components=1)
+        pca.fit(pair_diffs)
+        
+        gender_direction = pca.components_[0]
+    else:
+        print("No definitional pairs found in the vocabulary. Cannot calculate gender direction.")
+        return
+
+    male_average_vector = np.mean(male_vector_list, axis=0)
+    female_average_vector = np.mean(female_vector_list, axis=0)
+
+    distances_from_male, distances_from_female = [], []
+    direct_bias = []
+    all_words_direct_bias = {}
+    for word in words_for_bias_calculation:
+        word_index = tfidf_vectorizer.vocabulary_[word]
+        word_vector = tfidf_matrix[:, word_index].toarray().flatten()
+
+        dist_from_male = 1 - cosine_similarity([word_vector], [male_average_vector])[0][0]
+        dist_from_female = 1 - cosine_similarity([word_vector], [female_average_vector])[0][0]
+
+        distances_from_male.append(dist_from_male)
+        distances_from_female.append(dist_from_female)
+
+        cosine_sim = np.abs(np.dot(word_vector, gender_direction) / 
+                            (np.linalg.norm(word_vector) * np.linalg.norm(gender_direction)))
+        direct_bias.append(cosine_sim)
+        all_words_direct_bias[word] = cosine_sim 
+    
+    overall_direct_bias = np.mean(direct_bias)
+    print("Overall Direct Bias (excluding unwanted words):", overall_direct_bias)
+
+    distances_from_male, distances_from_female = [], []
+    for keyword in keywords:
+        if keyword in tfidf_vectorizer.vocabulary_:
+            keyword_index = tfidf_vectorizer.vocabulary_[keyword]
+            keyword_vector = tfidf_matrix[:, keyword_index].toarray().flatten()
+
+            dist_from_male = 1 - cosine_similarity([keyword_vector], [male_average_vector])[0][0]
+            dist_from_female = 1 - cosine_similarity([keyword_vector], [female_average_vector])[0][0]
+
+            distances_from_male.append(dist_from_male)
+            distances_from_female.append(dist_from_female)
+    
+    plt.figure(figsize=(12, 12))
+
+    min_value = min(distances_from_male + distances_from_female)
+    max_value = max(distances_from_male + distances_from_female)
+    plt.xlim([min_value-0.005, max_value+0.005])
+    plt.ylim([min_value-0.005, max_value+0.005])
+
+    x_vals = np.linspace(-1.15, 1.15, 400)
+    y_vals = x_vals
+    plt.plot(x_vals, y_vals, color='black', linewidth=1)
+
+    plt.fill_between(x_vals, y_vals, 1.15, color='red', alpha=0.1)  
+    plt.fill_between(x_vals, -1.15, y_vals, color='blue', alpha=0.1) 
+
+    masculism_words = []
+    feminism_words = []
+
+    for i, word in enumerate(keywords):
+        if distances_from_male[i] == distances_from_female[i]:
+            color = 'black' 
+        elif distances_from_male[i] < distances_from_female[i]:
+            distances_from_male[i] 
+            masculism_words.append(word)
+            color = 'red' 
+        else: 
+            distances_from_female[i]
+            feminism_words.append(word)
+            color = 'blue' 
+        plt.scatter(distances_from_male[i], distances_from_female[i], c=color)
+        plt.text(distances_from_male[i]+0.001, distances_from_female[i], word, fontsize=11)
+
+    print("Masculism words: ", masculism_words)
+    print("Feminism words: ", feminism_words)
+
+    plt.xlabel('Cosine Distance to Average Male Terms\' Embeddings')
+    plt.ylabel('Cosine Distance to Average Female Terms\' Embeddings')
+    plt.title('Gender Bias Visualization for Selected Keywords')
+    plt.grid(True)
+    plt.show()
